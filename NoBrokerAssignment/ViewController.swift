e34c93e6c2cdc690e4f795903220da676356981d
crickabhi <abhinav@eckovation.com>
2017-05-15 11:49:56 +0530

    Initial Commit

---
+++ NoBrokerAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoBrokerAssignment
+//
+//  Created by Abhinav Mathur on 15/05/17.
+//  Copyright © 2017 Abhinav Mathur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
